ffbbea44fa39d077bafabb77be72c340a7a38d02
Soprodevento <Sashinka2803@gmail.com>
2024-03-11 15:21:44 +0300

    Поправил код соответсвенно требованиям задачи

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {1,2,3,4,5,6,7};
-        getSubArrayBetween(numbers,1,11);
+        int[] numbers = {1,2,3,4,5,6,7 };
+        printArray(numbers);
     }
 
-    public static int[] getSubArrayBetween(int[] numbers, int start, int end) {
+    public static void printArray(int[] numbers) {
         System.out.print("[");
-        boolean firstNumberPrinted = false;
-        for (int number : numbers) {
-            if (number >= start && number <= end) {
-                if (firstNumberPrinted) {
-                    System.out.print(", ");
-                }
-                System.out.print(number);
-                firstNumberPrinted = true;
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i]);
+            if (i < numbers.length - 1) {
+                System.out.print(", ");
             }
         }
         System.out.print("]");
-        return numbers;
     }
 }
