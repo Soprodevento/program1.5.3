4c6b6e605df82e1ad18cbd8c3fac99726b6f97f1
Soprodevento <Sashinka2803@gmail.com>
2024-03-12 21:13:17 +0300

    Поправил код соответсвенно требованиям задачи

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {1,2,3,4,5,6,7 };
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7};
         printArray(numbers);
     }
 
     public static void printArray(int[] numbers) {
         System.out.print("[");
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.print(numbers[i]);
-            if (i < numbers.length - 1) {
+        for (int number : numbers) {
+            System.out.print(number);
+            if (number != numbers[numbers.length - 1]) {
                 System.out.print(", ");
             }
         }
